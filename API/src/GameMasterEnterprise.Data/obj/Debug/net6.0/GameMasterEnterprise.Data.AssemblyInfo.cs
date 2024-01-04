3798007844a0e688ecd7c5eb71583e8f550cfafe
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2024-01-03 21:46:27 -0300

    main

--- API/src/GameMasterEnterprise.Data/obj/Debug/net6.0/GameMasterEnterprise.Data.AssemblyInfo.cs
+++ API/src/GameMasterEnterprise.Data/obj/Debug/net6.0/GameMasterEnterprise.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameMasterEnterprise.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2cabbec4f1116388b47ee11f2460facf6f242f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameMasterEnterprise.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameMasterEnterprise.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
