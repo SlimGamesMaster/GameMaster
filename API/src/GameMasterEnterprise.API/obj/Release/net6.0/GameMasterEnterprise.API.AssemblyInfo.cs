f3bb7134822677382cb7372004b0c4317f2009a4
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2024-02-15 22:56:45 -0300

    Fix url

--- API/src/GameMasterEnterprise.API/obj/Release/net6.0/GameMasterEnterprise.API.AssemblyInfo.cs
+++ API/src/GameMasterEnterprise.API/obj/Release/net6.0/GameMasterEnterprise.API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameMasterEnterprise.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0afd030f72e79d257ba14e480cc90188ff429c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5458aaf0176639515ea076146e4514104c94596a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameMasterEnterprise.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameMasterEnterprise.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
